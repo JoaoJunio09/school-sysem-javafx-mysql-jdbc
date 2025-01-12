925df23a74e880b4154400e02344853a6a07fc36
joao_junio <joaojunio818@gmail.com>
2025-01-12 17:40:15 -0300

    Modificando a classe main

--- src/main/java/br/com/schoolsystem/HelloApplication.java
+++ src/main/java/br/com/schoolsystem/HelloApplication.java
@@ -35,7 +35,6 @@ public class HelloApplication extends Application {
 
     public static Scene getMainScene() { return mainScene; }
 
-
     public static void main(String[] args) {
         launch();
     }
